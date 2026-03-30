5484d52ea0c4a76c24bb2714843be2f9cff64fce
ysjsjsjsjidhe <disinidisaa@gmail.com>
2026-03-31 06:34:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --- KONFIGURASI API (Ganti dengan API Key milikmu) ---
-const GEMINI_API_KEY = "MASUKKAN_API_KEY_KAMU_DI_SINI"; 
+const GEMINI_API_KEY = "AIzaSyB7KzKcaEWhLPaWjKQthOyeW2Pp8Bgx1RY"; 
 const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + GEMINI_API_KEY;
 
 // Database Foto
